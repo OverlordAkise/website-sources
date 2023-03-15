83337e1d74b88847234bf58a56b8d3905b230102
OverlordAkise <OverlordAkise@gmail.com>
2023-03-15 21:26:27 +0100

    Updated luctus.at to different template

--- luctus_at/files/sv_thirdperson_fix.lua
+++
@@ -1,7 +0,0 @@
---Luctus Thirdperson Wallhack Fix
---Made by OverlordAkise
-
-hook.Add("PlayerInitialSpawn", "luctus_fix_thirdperson", function(ply)
-  RunConsoleCommand("simple_thirdperson_forcecollide",1)
-  hook.Remove("PlayerInitialSpawn", "luctus_fix_thirdperson")
-end)
